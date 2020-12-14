bd184884a18bf716c1d34c645e80745408f4d94e
loulou <yourdj.france@gmail.com>
2020-12-14 09:57:27 +0100

    Quête Middleware In Express CHALLENGE

--- index.jsx
+++ index.jsx
@@ -1,16 +1,149 @@
 const connection = require("./src/config");
 const express = require("express");
 const movies = require("./movies");
+const users = require("./users");
+
 const port = 3000;
 const app = express();
-const url = require("url");
 
-app.get("/", (request, response) => {
+app.use(express.json()) // For JSON format
+
+function logInfos(req, res, next) {
+  console.log(`${req.method} request from ${req.hostname}`);
+  next();
+}
+
+app.use(logInfos); 
+
+// --- DELETE METHOD ---
+// The ID is passed as a route parameter
+app.delete("/api/users/:id", (req, res) => {
+  const idUser = req.params.id; 
+  connection.query("DELETE FROM users WHERE id = ?", [idUser], (err) => {
+    // TODO send a response to the client (step 3)
+    if (err) {
+      console.log(err);
+      res.status(500).send("😱 Error deleting an user");
+    } else {
+      res.status(200).send("🎉 User deleted!");
+    }
+  });
+});
+
+app.delete("/api/movies/:id", (req, res) => {
+  const idMovie = req.params.id; 
+  connection.query("DELETE FROM movies WHERE id = ?", [idMovie], (err) => {
+    // TODO send a response to the client (step 3)
+    if (err) {
+      console.log(err);
+      res.status(500).send("😱 Error deleting an user");
+    } else {
+      res.status(200).send("🎉 User deleted!");
+    }
+  });
+});
+
+// --- PUT METHOD ---
+// This route will update a user in the DB
+app.put("/api/users/:id", (req, res) => {
+  // We get the ID from the url:
+  const idUser = req.params.id;
+
+  // We get the data from the req.body
+  const newUser = req.body;
+  console.log(idUser)
+  console.log(newUser)
+  // We send a UPDATE query to the DB
+  connection.query(
+    "UPDATE users SET ? WHERE id= ? ;",
+    // "UPDATE users SET lastname=lastname, firstname=firstname, email=email WHERE id=idUser;",
+    [newUser, idUser],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error updating a user");
+      } else {
+        res.status(200).send("User updated successfully 🎉");
+      }
+    }
+  );
+});
+
+app.put("/api/movies/:id", (req, res) => {
+  // We get the ID from the url:
+  const idMovie = req.params.id;
+  const title = req.params.title;
+  const director = req.params.director;
+  const year = req.params.year;
+  const color = req.params.color;
+  const duration = req.params.duration;
+
+  // We get the data from the req.body
+  const newMovie = req.body;
+  
+  // We send a UPDATE query to the DB
+  connection.query(
+    "UPDATE movies SET ? WHERE id= ? ;",
+    [newMovie, idMovie, title, director, year, color, duration ],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error updating a movie");
+      } else {
+        res.status(200).send("Movie updated successfully 🎉");
+      }
+    }
+  );
+});
+
+// --- POST METHOD ---
+app.post("/api/movies", (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  connection.query(
+    "INSERT INTO movies(title, director, year, color, duration) VALUES(?, ?, ?, ?, ?)",
+    [title, director, year, color, duration], 
+    (err, results) => {
+          if (err) {
+            console.log(err);
+            res.status(500).send("Error saving a movie");
+          } else {
+            res.status(200).send("Successfully saved");
+          }
+        }
+    ); 
+});
+
+app.post("/api/users", (req, res) => {
+  const { firstname, lastname, email } = req.body;
+  connection.query(
+    "INSERT INTO users(firstname, lastname, email) VALUES(?, ?, ?)",
+    [firstname, lastname, email], 
+    (err, results) => {
+          if (err) {
+            console.log(err);
+            res.status(500).send("Error saving a user");
+          } else {
+            res.status(200).send("Successfully saved");
+          }
+        }
+    ); 
+});
+
+// --- GET METHOD ---
+
+app.get("/superMiddleware", (request, response, next) => {
+  console.log("Hello middleware")
+  next()
+}, function (request, response) {
+  response.send("Hello world");
+})
+
+app.get("/api", (request, response) => {
   console.log(request);
   response.send("Welcome to my favourite movie list");
 });
 
-app.get("/movies", (request, response) => {
+app.get("/api/movies", (request, response) => {
   console.log("request ==> " +request);
   // response.status(200).json(movies);
   connection.query("SELECT * from movies", (err, results) => {
@@ -22,7 +155,7 @@ app.get("/movies", (request, response) => {
   });
 });
 
-app.get("/movies/:id", (request, response) => {
+app.get("/api/movies/:id", (request, response) => {
   console.log("request.url ==> " +request.url)
   console.log("request.params.id ==> " +request.params.id) 
   if (request.params.id > 5) {
@@ -43,12 +176,12 @@ app.get("/movies/:id", (request, response) => {
   // response.status(200).json(movies[id]);
 });
 
-app.get("/movies/5", (request, response) => {
+app.get("/api/movies/5", (request, response) => {
   console.log(request);
   response.send("Not found");
 });
 
-app.get("/search/", (request, response) => {
+app.get("/api/search/", (request, response) => {
   console.log("request.query.maxDuration ==> " +request.query.maxDuration);
   const maxDuration = request.query.maxDuration;
   const result = movies.filter(movie => movie.duration <= maxDuration);
@@ -68,9 +201,17 @@ app.get("/search/", (request, response) => {
   }
 });
 
-app.get("/users", (request, response) => {
-  console.log(request);
-  response.send("unauthorized");
+app.get("/api/users", (request, response) => {
+  // console.log(request);
+  // response.send("unauthorized");
+  console.log("request ==> " +request);
+  connection.query("SELECT * from users", (err, results) => {
+    if (err) {
+      response.status(500).send("Error retrieving data");
+    } else {
+      response.status(200).json(results);
+    }
+  });
 });
 
 app.listen(port, () => {
---
+++ users.js
@@ -0,0 +1,10 @@
+const users = [
+    {
+        id: 0,
+        firstname: "Citizen Kane",
+        lastname: "Orson Wells",
+        email: "1941"
+      }
+  ];
+  
+module.exports = users;  
